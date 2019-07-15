50b5f1198024c7dea9ca6fc5648cb65bea6fedc6
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-15 11:03:53 +0100

    Conditionally applying classes

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part" :style="headBorderStyle">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{ selectedRobot.head.title }}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -83,9 +83,14 @@ export default {
     };
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
     headBorderStyle() {
       return {
-        border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa',
+        border: this.selectedRobot.head.onSale
+          ? '3px solid red'
+          : '3px solid #aaa',
       };
     },
     selectedRobot() {
@@ -267,5 +272,8 @@ export default {
   }
   .cost {
     text-align: right;
+  }
+  .sale-border {
+    border: 3px solid red;
   }
 </style>
