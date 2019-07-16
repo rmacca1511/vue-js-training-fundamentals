fcc55cce51a63789981ef6b7be27b9599162bb6f
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-16 12:09:06 +0100

    Using Directive Lifecycle Hooks

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="part" :class="position" >
+    {{ pinPadding }}
     <router-link :to="{
         name: 'Parts',
         params: {
@@ -11,8 +12,10 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin="{ bottom: '10px', right: '50px' }" class="sale"
-    v-show="selectedPart.onSale">Sale!</span>
+    <span
+      @click="pinPadding='30px'"
+      v-pin="{ bottom: pinPadding, right: pinPadding }" class="sale"
+      v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -45,7 +48,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
