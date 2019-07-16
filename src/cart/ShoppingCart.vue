4f429ff78c78951cc6302a36406700b97264fefc
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-16 10:39:31 +0100

    Added Sale and Accessing Namespaced Getters

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,31 @@
         </tr>
       </tbody>
     </table>
+    <br />
+    <br />
+    <h2 class="sale-title">You saved money on these robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">
+            {{robot.head.title}}
+          </td>
+          <td class="cost">
+            {{robot.cost}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
  </div>
 </template>
 
@@ -32,6 +57,9 @@ export default {
   computed: {
     cart() {
       return this.$store.state.robots.cart;
+    },
+    cartSaleItems() {
+      return this.$store.getters['robots/cartSaleItems'];
     },
   },
 };
@@ -47,5 +75,8 @@ export default {
   }
   .cost {
     text-align: right;
+  }
+  .sale-title {
+    color: red;
   }
 </style>
